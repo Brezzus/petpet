d4962319efd20b47d9416cc4cce347c701722b10
CAUA LUIZ SILVA BRESCHI <CG3037517@car.ifsp>
2026-03-18 08:26:20 -0300

    ajustando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -11,6 +13,7 @@ export const home = (req: Request, res: Response) => {
 
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+         menu: createMenuObject('all'),
         banner: {
             title: 'dogs',
             background: 'banner_dog.jpg'
@@ -20,6 +23,7 @@ export const dogs = (req: Request, res: Response) => {
 
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'cats',
             background: 'banner_cat.jpg'
@@ -29,9 +33,11 @@ export const cats = (req: Request, res: Response) => {
 
 export const fishes = (req: Request, res: Response) => {
     res.render('pages/page', {
+             menu: createMenuObject('all'),
         banner: {
             title: 'fishes',
             background: 'banner_fish.jpg'
         }
     });
 };
+
